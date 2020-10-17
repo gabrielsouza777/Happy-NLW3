e860326406c1aea048226471b7ad6d0c40375fbc
gabriel.souza.7 <gabriel.souza.7@hotmail.com>
2020-10-17 12:32:31 -0300

    2º Dia - Back-end Completo

---
+++ backend/src/routes.ts
@@ -0,0 +1,12 @@
+import { Router} from 'express'
+import multer from 'multer'
+import uploadConfig from './config/upload'
+import OrphanagesController from './controllers/OrphanagesController'
+
+const upload = multer(uploadConfig)
+const routes = Router()
+
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create)
+routes.get('/orphanages', OrphanagesController.index)
+routes.get('/orphanages/:id', OrphanagesController.show)
+export default routes
