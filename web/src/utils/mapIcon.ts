643f3641b61299160b888f949d5243a47308b5c8
gabriel.souza.7 <gabriel.souza.7@hotmail.com>
2020-10-19 13:49:58 -0300

    3º Dia - Front-end completo

---
+++ web/src/utils/mapIcon.ts
@@ -0,0 +1,11 @@
+import mapMarkerImg from '../images/map-marker.svg';
+import L from 'leaflet';
+const mapIcon = L.icon({
+    iconUrl: mapMarkerImg,
+  
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [0, -60]
+  })
+
+  export default mapIcon
